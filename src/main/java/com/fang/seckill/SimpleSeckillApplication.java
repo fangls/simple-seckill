00c555c118f87c0fc5995de8007641a49c074551
fangliangsheng <fangliangsheng@guohuaigroup.com>
2018-12-18 18:00:58 +0800

    创建工程

---
+++ src/main/java/com/fang/seckill/SimpleSeckillApplication.java
@@ -0,0 +1,14 @@
+package com.fang.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSeckillApplication.class, args);
+    }
+
+}
+
